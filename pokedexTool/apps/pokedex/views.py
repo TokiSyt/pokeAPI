a8c92b4bd6ed5c16944766281b6d7ae14dbbde83
TokiSyt <yuutokei30@gmail.com>
2025-10-07 14:10:13 +0200

    LoginRequired added

--- pokedexTool/apps/pokedex/views.py
+++ pokedexTool/apps/pokedex/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-# Create your views here.
-
-class pokedexHomeView(TemplateView):
+class pokedexHomeView(LoginRequiredMixin, TemplateView):
     template_name = "pokedex/pokedex-home.html"
     
