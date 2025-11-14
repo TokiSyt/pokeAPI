16110695d72583600fb1b8ea480c508fc8d3f2f5
TokiSyt <yuutokei30@gmail.com>
2025-11-14 12:23:23 +0100

    updating admin models

---
+++ pokedexTool/apps/poke_types/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import PokemonType, TypeDamageRelation
+
+# Register your models here.
+
+admin.site.register(PokemonType)
+admin.site.register(TypeDamageRelation)
--- pokedexTool/apps/pokemons/admin.py
+++ pokedexTool/apps/pokemons/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Pokemon, PokemonType
+from .models import Pokemon,  PokemonTypeRelation, PokemonAbilityRelation
 
 # Register your models here.
 
 admin.site.register(Pokemon)
-admin.site.register(PokemonType)
+admin.site.register(PokemonTypeRelation)
+admin.site.register(PokemonAbilityRelation)
