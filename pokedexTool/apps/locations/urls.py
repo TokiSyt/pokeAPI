f23fd325dc996ddbbc0508976b13c7965a410e19
TokiSyt <yuutokei30@gmail.com>
2025-11-14 12:21:08 +0100

    adding new apps

---
+++ pokedexTool/apps/locations/urls.py
@@ -0,0 +1,9 @@
+from .views import MovesSearchView
+from django.urls import path
+
+app_name = "types"
+
+urlpatterns = [
+    path("", MovesSearchView.as_view(), name="locations-search"),
+    path("<str:pokemon_type>", MovesSearchView.as_view(), name="locations-detail"),
+]
