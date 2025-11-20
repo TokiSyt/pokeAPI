c6a0a69af8fd0af768c96ad8ad8330079e0a459b
TokiSyt <yuutokei30@gmail.com>
2025-11-20 18:42:35 +0100

    fixed duplicated url namespace

--- pokedexTool/apps/berries/urls.py
+++ pokedexTool/apps/berries/urls.py
@@ -1,7 +1,7 @@
 from .views import MovesSearchView
 from django.urls import path
 
-app_name = "types"
+app_name = "berries"
 
 urlpatterns = [
     path("", MovesSearchView.as_view(), name="berries-search"),
